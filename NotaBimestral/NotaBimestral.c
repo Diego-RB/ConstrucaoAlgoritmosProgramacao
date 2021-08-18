f327cff7475868cb32b8c8d14ad48ea6cbb39631
Diego-RB <63828358+Diego-RB@users.noreply.github.com>
2021-08-17 21:38:05 -0300

    Exercicio 1 completo

--- NotaBimestral/NotaBimestral.c
+++ NotaBimestral/NotaBimestral.c
@@ -1,13 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 //1.	Faça um Programa que peça as 4 notas bimestrais e mostre a média.
-void main() {
+int main() {
     double nota, media, soma;
     for(double i = 1; i <= 4; i++){
-        printf("Digita a nota do %d Bimestre: ",i);
-        scanf("%f",&nota);
-        soma += soma + nota;
+        printf("Digita a nota do %f Bimestre: ",i);
+        scanf("%lf",&nota);
+        soma = soma + nota;
     }
     media = soma/4;
     printf("A media eh : %f",media);
+    printf("A some eh : %f",soma);
+    return 0;
 }
